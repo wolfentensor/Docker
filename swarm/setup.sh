c24b3cdea60496c2bf0e4d1eeca98fda43fe0a1b
wolfentensor <greogry@opentensor.dev>
2024-03-25 10:02:37 -0600

    moved stuff

--- swarm/setup.sh
+++ swarm/setup.sh
@@ -3,8 +3,40 @@
 # Run Subtensor (Substrate) under gVisor
 # Only supports Docker standalone mode for now.
 #
-source ../extras/ensure_deps.sh
 
+ensure_dependencies() {
+  local missing_deps=()
+  for dep in "$@"; do
+    if ! command -v "$dep" &> /dev/null; then
+      missing_deps+=("$dep")
+    fi
+  done
+  if [ ${#missing_deps[@]} -ne 0 ]; then
+    echo "The following dependencies are missing: ${missing_deps[*]}"
+    echo "Attempting to install missing dependencies..."
+    sudo apt-get update
+    sudo apt-get install -y "${missing_deps[@]}"
+  else
+    echo "All dependencies are met."
+    return 0
+  fi
+}
+
+ensure_bindeps() {
+  local missing_bins=()
+  for bin in "$@"; do
+    if ! command -v "$bin" &> /dev/null; then
+      missing_bins+=("$bin")
+    fi
+  done
+  if [ ${#missing_bins[@]} -ne 0 ]; then
+    echo "The following debindencies are missing: ${missing_bins[*]}"
+    return 1
+  else
+    echo "All debindencies are met."
+    return 0
+  fi
+}
 
 install_runsc() {
   wget https://storage.googleapis.com/gvisor/releases/release/latest/x86_64/runsc
